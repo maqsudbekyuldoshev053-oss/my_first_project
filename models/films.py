75eeadb0db8cc7ea706ded4b6e4d7aac2582307c
maqsudbek <maqsudbekyuldoshev053@gmail.com>
2026-02-15 18:43:34 +0500

    first_project

--- models/films.py
+++ models/films.py
@@ -1,25 +1,14 @@
-
-
-from sqlalchemy import String, ForeignKey, Table, Column, Integer,UniqueConstraint, Text
+from sqlalchemy import String, ForeignKey, Integer, UniqueConstraint, Text
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 
-from models.base import Model, CreatedBaseModel, Base
+from models.base import Model
 
-
-
-# film_category = Table(
-#     "film_category",
-#     Base.metadata,
-#     Column("film_id", ForeignKey("films.id"), primary_key=True),
-#     Column("category_id", ForeignKey("categories.id"), primary_key=True),
-# )
 
 class Film(Model):
     title: Mapped[str] = mapped_column(String(255))
     description: Mapped[str] = mapped_column(Text, nullable=True)
     release_year: Mapped[int] = mapped_column(Integer, nullable=True)
     categories: Mapped[list['Category']] = relationship('Category', secondary='film_categories', back_populates='films')
-
 
     def __str__(self):
         return f"{self.id} - {self.title}"
@@ -28,7 +17,6 @@ class Film(Model):
 class Category(Model):
     name: Mapped[str] = mapped_column(String(25))
     films: Mapped[list['Film']] = relationship('Film', secondary='film_categories', back_populates='categories')
-
 
     def __str__(self):
         return f"{self.id} - {self.name}"
@@ -44,5 +32,3 @@ class FilmCategory(Model):
 
     def __str__(self):
         return f'id: {self.id}, category_id: {self.category_id}, film_id: {self.film_id}'
-
-
