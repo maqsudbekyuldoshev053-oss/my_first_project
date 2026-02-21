b2083bfacbb5f8d3b99d5eed7244a3e230f68f99
maqsudbek <maqsudbekyuldoshev053@gmail.com>
2026-02-21 19:52:13 +0500

    first_project

--- main.py
+++ main.py
@@ -1,436 +1,114 @@
-from sqlalchemy import delete, insert, select, update
-from sqlalchemy.orm import Session
+import asyncio
+from aiogram import Bot, Dispatcher, Router, F
+from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.types import Message, KeyboardButton, BotCommand, BufferedInputFile, InputProfilePhotoStatic, ReplyKeyboardRemove
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
-# from models.base import engine
-
-# from models.courses import Course
-# from models.users import User
-from models.films import Film
-
-# Base.metadata.create_all(engine)
-
-# with Session(engine) as session:
-#     data = [
-#         {
-#             'id': 1,
-#             'name': 'Texnika',
-#             'slug': 'texnika'
-#         }
-#     ]
-#     query = insert(Category).values(data)
-#     session.execute(query)
-#     session.commit()
-#
-# with Session(engine) as session:
-#     while True:
-#         print("""
-#     1.Kurs qo'shish
-#     2.Kurs o'chirish
-#     3.Kursni o'zgartirish
-#     4.Kurslarni ko'rish
-#     5.Student qo'shish
-#     6.Student o'chirish
-#     7.Studentni o'zhartirish
-#     8.Studentlarni ko'rish
-#     0.Exit
-#     """)
-#
-#         t = input('Tanlang: ')
-#         if t == '1':
-#             name = input('Kursning nomi: ')
-#             session.execute(insert(Course).values({"name": name}))
-#             session.commit()
-#
-#         elif t == '2':
-#             _id = int(input("O‘chiriladigan kurs id: "))
-#             session.execute(delete(Course).filter(Course.id == _id))
-#             session.commit()
-#
-#         elif t == '3':
-#             _id = int(input("O'zgartiriladigan kurs id: "))
-#             new_name = input("Kursning yangi nomi: ")
-#             query = update(Course).filter(Course.id == _id).values({"name": new_name})
-#             session.execute(query)
-#             session.commit()
-#
-#         elif t == '4':
-#             courses = session.execute(select(Course.id, Course.name).order_by(Course.id))
-#             for course in courses:
-#                 print(f"{course.id}. {course.name}")
-#
-#         elif t == '5':
-#             name = input('Studentning ismi: ')
-#             phone = input('Studentning phone ni: ')
-#             session.execute(insert(User).values({"first_name": name, "phone": phone}))
-#             session.commit()
-#
-#         elif t == '6':
-#             _id = int(input("O‘chiriladigan studentning id si: "))
-#             session.execute(delete(User).filter(User.id == _id))
-#             session.commit()
-#
-#         elif t == '7':
-#             _id = int(input("O'zgartiriladigan studentning id si: "))
-#             new_name = input("Studentning yangi ismi: ")
-#             new_phone = input("Studentning yangi phone ni: ")
-#             session.execute(update(User).filter(User.id == _id).values({"first_name": new_name, "phone": new_phone}))
-#             session.commit()
-#
-#         elif t == '8':
-#             students = session.execute(select(User.id, User.first_name, User.phone).order_by(User.id))
-#             for student in students:
-#                 print(f"{student.id}. {student.first_name} ({student.phone})")
-#
-#         elif t == '0':
-#             break
-
-#
-# with Session(engine) as session:
-#     while True:
-#         print("""
-#     Menyular
-#         1. kurs qo'shish
-#         2. kurs o'chirish
-#         3. kursni o'zgartirish
-#         4. kurslarni ko'rish
-#         5. student qo'shish
-#         6. student o'chirish
-#         7. studentni o'zgartirish
-#         8. kursga student qo'shish
-#         9. kursdan studentni o'chirish
-#         10. kursdan kursga ko'chirish
-#         0.Exit
-#     """)
-#
-#         t = input('Tanlang: ')
-#         if t == '1':
-#             name = input('Kursning nomi: ')
-#             session.execute(insert(Course).values({"name": name}))
-#             session.commit()
-#
-#         elif t == '2':
-#             _id = int(input("O‘chiriladigan kurs id: "))
-#             session.execute(delete(Course).filter(Course.id == _id))
-#             session.commit()
-#         elif t == '3':
-#             _id = int(input("O'zgartiriladigan kurs id: "))
-#             new_name = input("Kursning yangi nomi: ")
-#             query = update(Course).filter(Course.id == _id).values({"name": new_name})
-#             session.execute(query)
-#             session.commit()
-#
-#         elif t == '4':
-#             courses = session.execute(select(Course.id, Course.name).order_by(Course.id))
-#             for course in courses:
-#                 print(f"{course.id}. {course.name}")
-#
-#         elif t == '5':
-#             name = input('Studentning ismi: ')
-#             phone = input('Studentning phone ni: ')
-#             session.execute(insert(User).values({"first_name": name, "phone": phone}))
-#             session.commit()
-#
-#         elif t == '6':
-#             _id = int(input("O‘chiriladigan studentning id si: "))
-#             session.execute(delete(User).filter(User.id == _id))
-#             session.commit()
-#
-#         elif t == '7':
-#             _id = int(input("O'zgartiriladigan studentning id si: "))
-#             new_name = input("Studentning yangi ismi: ")
-#             new_phone = input("Studentning yangi phone ni: ")
-#             session.execute(update(User).filter(User.id == _id).values({"first_name": new_name, "phone": new_phone}))
-#             session.commit()
-#
-#
-#         elif t == '8':
-#             student_id = int(input("Kursga qo'shmoqchi bulgan studentning id si: "))
-#             course_id = int(input("Student qo'shmoqchi bulgan kurs id si: "))
-#             query = select(User).filter(User.id == student_id)
-#             student = session.scalar(query)
-#             if student is None:
-#                 print("Student topilmadi.")
-#             else:
-#                 query = select(Course).filter(Course.id == course_id)
-#                 course = session.scalar(query)
-#                 if course is None:
-#                     print("Kurs topilmadi.")
-#                 elif course in student.courses:
-#                     print("Student allaqachon ushbu coursega qo'shilgan.")
-#                 else:
-#                     student.courses.append(course)
-#                     session.commit()
-#
-#         elif t == '9':
-#             student_id = int(input("Kursdan o'chiriladigan studentning id si: "))
-#             course_id = int(input("Student o'chiriladigan course id si: "))
-#             query2 = select(User).filter(User.id == student_id)
-#             student = session.scalar(query2)
-#             if student is None:
-#                 print("Student topilmadi.")
-#             else:
-#                 query2 = select(Course).filter(Course.id == course_id)
-#                 course = session.scalar(query2)
-#                 if course is None:
-#                     print("Kurs topilmadi.")
-#                 elif course not in student.courses:
-#                     print("Student bu coursega biriktirilmagan.")
-#                 else:
-#                     student.courses.remove(course)
-#                     session.commit()
-#
-#         elif t == '10':
-#             student_id = int(input("Ko'chiriladigan studentning id si: "))
-#             from_course_id = int(input("Qaysi coursedan: "))
-#             to_course_id = int(input("Qaysi coursega: "))
-#             query3 = select(User).filter(User.id == student_id)
-#             student = session.scalar(query3)
-#             if student is None:
-#                 print("Student topilmadi.")
-#             else:
-#                 query3 = select(Course).filter(Course.id == from_course_id)
-#                 from_course = session.scalar(query3)
-#                 query3 = select(Course).filter(Course.id == to_course_id)
-#                 to_course = session.scalar(query3)
-#                 if from_course is None:
-#                     print("Eski course topilmadi.")
-#                 elif to_course is None:
-#                     print("Yangi course topilmadi.")
-#                 elif from_course not in student.courses:
-#                     print("Student eski coursega biriktirilmagan.")
-#                 elif to_course in student.courses:
-#                     print("Student allaqachon yangi coursega biriktirilgan.")
-#                 else:
-#                     student.courses.remove(from_course)
-#                     student.courses.append(to_course)
-#                     session.commit()
-#
-#         elif t == '0':
-#             break
-
-# Film(id, title, description, release_year), Category(id, name)
-#
-# with Session(engine)  as session:
-#     while True:
-#         """
-# 1. film qoshish
-# 2. film o'chirish
-# 3. film o'zgartirish
-# 4. film ni korish
-# 5. category qo'shish
-# 6. category o'chirish
-# 7. category o'zgartirish
-# 8. category ni korish
-# """
-#
-#         t = input('Tanlang: ')
-#         if t == '1':
-#             title = input('Film title: ')
-#             session.execute(insert(Film).values({"title": title}))
-#             session.commit()
-#
-#         elif t == '2':
-#             _id = int(input("Film id: "))
-#             session.execute(delete(Film).filter(Film.id == _id))
-#             session.commit()
-#
-#         elif t == '3':
-#             _id = int(input("Film id: "))
-#             new_title = input("Film new title: ")
-#             query = update(Film).filter(Film.id == _id).values({"title": new_title})
-#             session.execute(query)
-#             session.commit()
-#
-#         elif t == '4':
-#             films = session.execute(select(Film.id, Film.title).order_by(Film.id))
-#             for film in films:
-#                 print(f"{film.id}. {film.title}")
-#
-#         elif t == '5':
-#             name = input('Category name: ')
-#             session.execute(insert(Category).values({"first_name": name}))
-#             session.commit()
-#
-#         elif t == '6':
-#             _id = int(input("Category id : "))
-#             session.execute(delete(Category).filter(Category.id == _id))
-#             session.commit()
-#
-#         elif t == '7':
-#             _id = int(input("Category id : "))
-#             new_name = input("Category new name: ")
-#             session.execute(update(Category).filter(Category.id == _id).values({"first_name": new_name}))
-#             session.commit()
-#
-#         elif t == '8':
-#             categories = session.execute(select(Category.id, Category.name).order_by(Category.id))
-#             for category in categories:
-#                 print(f"{category.id}. {category.first_name} ")
-#
-#         elif t == '0':
-#             break
-
-#
-# with Session(engine) as session:
-#     while True:
-#         print("""
-#     1. film qoshish
-#     2. film o'chirish
-#     3. film o'zgartirish
-#     4. film ni korish
-#     5. category qo'shish
-#     6. category o'chirish
-#     7. category o'zgartirish
-#     8. category ni korish
-#     """)
-#
-#         t = input('Tanlang: ')
-#         if t == '1':
-#             title = input('Film title: ')
-#             description = input('Film description: ')
-#             release_year = int(input('Film release_year: '))
-#             query = (insert(Film).values(title=title, description=description, release_year=release_year ))
-#             session.execute(query)
-#             session.commit()
-#
-#         elif t == '2':
-#             _id = int(input("Film id: "))
-#             query = (delete(Film).filter(Film.id == _id))
-#             session.execute(query)
-#             session.commit()
-#
-#         elif t == '3':
-#             _id = int(input("Film id: "))
-#             new_title = input("Film new title: ")
-#             new_description = input('Film description: ')
-#             new_release_year = int(input('Film release_year: '))
-#             query = update(Film).filter(Film.id == _id).values(title=new_title, description=new_description, release_year=new_release_year)
-#             session.execute(query)
-#             session.commit()
-#
-#         elif t == '4':
-#             films = session.execute(select(Film.id, Film.title).order_by(Film.id))
-#             if not films:
-#                 print("Film topilmadi!!!")
-#             else:
-#                 for film in films:
-#                     print(f"{film.id}. {film.title}")
-#
-#         elif t == '5':
-#             name = input('Category name: ')
-#             query = (insert(Category).values({"name": name}))
-#             session.execute(query)
-#             session.commit()
-#
-#         elif t == '6':
-#             _id = int(input("Category id: "))
-#             query = (delete(Category).filter(Category.id == _id))
-#             session.execute(query)
-#             session.commit()
-#
-#         elif t == '7':
-#             _id = int(input("Category id: "))
-#             new_name = input("Category new name: ")
-#             query = (update(Category).filter(Category.id == _id).values({"name": new_name}))
-#             session.execute(query)
-#             session.commit()
-#
-#         elif t == '8':
-#             query = (select(Category.id, Category.name).order_by(Category.id))
-#             categories = session.execute(query)
-#             for category in categories:
-#                 print(f"{category.id}. {category.name} ")
-#
-#         elif t == '0':
-#             break
+bot = Bot('7874681745:AAFb_km4wx4lHNfD-MAYK9OmabOhLdB0ORs')
 
 
-if __name__ == '__main__':
-    while True:
-        print("""
-        1. film read
-        2. film update
-        3. film delete
-        4. film create
-       """)
-        choose = input("Enter your choice: ")
-        if choose == '1':
-            for i in Film.get_all():
-                print(i)
-        elif choose == '2':
-            film_id = input("Enter your film id title which you want to update: ")
-            title = input("Changing Film Title: ")
-            description = input("Changing Film Description: ")
-            release_year = input("Changing Film Release Year: ")
-            data = {
-                "title": title,
-                "description": description,
-                "release_year": release_year
-            }
-            Film.update(film_id, **data)
-        elif choose == '3':
-            film_id = input("Enter your film id title which you want to delete: ")
-            Film.delete(film_id)
-        elif choose == '4':
-            title = input("Film Title: ")
-            description = input("Film Description: ")
-            release_year = input("Film Release Year: ")
-            data = {
-                "title": title,
-                "description": description,
-                "release_year": release_year
-            }
-            Film.create(**data)
-            print('Qoshildi')
+dp = Dispatcher(storage=MemoryStorage())
+router = Router()
+dp.include_router(router)
 
 
+class Setting(StatesGroup):
+    set_name = State()
+    set_short_description = State()
+    set_profile_photo = State()
+    set_long_description = State()
 
 
+@dp.message(Command('start'))
+async def handle_start(message: Message):
+    rkm = ReplyKeyboardBuilder()
+    rkm.add(
+        KeyboardButton(text='Set name', style='danger'),
+        KeyboardButton(text='Set Short Description', style='primary'),
+        KeyboardButton(text='Set Profile Photo', style='success'),
+        KeyboardButton(text='Set Long Description', style='danger')
+    )
+    rkm.adjust(2)
+    await message.answer('Xush kelibsiz', reply_markup=rkm.as_markup(resize_keyboard=True))
 
 
+@dp.message(lambda msg: msg.text == 'Set name')
+async def handle_new_name(message: Message, state: FSMContext):
+    await message.answer('Yangi nom kiriting: ', reply_markup=ReplyKeyboardRemove())
+    await state.set_state(Setting.set_name)
 
 
+@dp.message(Setting.set_name)
+async def changing_name(message: Message, bot: Bot, state: FSMContext):
+    await state.clear()
+    await bot.set_my_name(message.text)
+
+    await message.answer("Botning nomi updated boldi ✅")
 
 
+@dp.message(lambda msg: msg.text == 'Set Short Description')
+async def handle_new_description(message: Message, state: FSMContext):
+    await message.answer('Yangi qisqa description kiriting: ', reply_markup=ReplyKeyboardRemove())
+    await state.set_state(Setting.set_short_description)
 
 
+@dp.message(Setting.set_short_description)
+async def changing_description(message: Message, bot: Bot, state: FSMContext):
+    await state.clear()
+    await bot.set_my_short_description(message.text)
+    await message.answer('Description muvaffaqiyatli ozgartirildi✅')
 
 
+@dp.message(lambda msg: msg.text == 'Set Profile Photo')
+async def handle_new_description(message: Message, state: FSMContext):
+    await message.answer('Profil uchun rasm yuboring: ', reply_markup=ReplyKeyboardRemove())
+    await state.set_state(Setting.set_profile_photo)
 
 
+@dp.message(Setting.set_profile_photo)
+async def changing_description(message: Message, bot: Bot, state: FSMContext):
+    if not message.photo:
+        await message.answer('Rasm yuboring', reply_markup=ReplyKeyboardRemove())
+        return
+
+    photo = message.photo[-1]
+
+    file = await bot.get_file(photo.file_id)
+    file_bytes = await bot.download_file(file.file_path)
+
+    input_file = BufferedInputFile(
+        file_bytes.read(),
+        filename="profile.jpg"
+    )
+    profile_photo_ = InputProfilePhotoStatic(photo=input_file)
+    await bot.set_my_profile_photo(photo=profile_photo_)
+
+    await message.answer("Profil rasm muvaffaqiyatli ozgartirildi ✅")
+    await state.clear()
+
+@dp.message(lambda msg: msg.text == 'Set Long Description')
+async def handle_long_description(message: Message, state: FSMContext):
+    await message.answer('Bot uchun uzun description yozing: ', reply_markup=ReplyKeyboardRemove())
+    await state.set_state(Setting.set_long_description)
+
+@dp.message(Setting.set_long_description)
+async def changing_long_description(message: Message, bot: Bot, state: FSMContext):
+    await state.clear()
+    await bot.set_my_description(message.text)
+    await message.answer('Long description ozgartirildi ✅')
 
 
+async def startup(bot: Bot):
+    await bot.set_my_commands([BotCommand(command='start', description='start')])
 
 
+async def main() -> None:
+    dp.startup.register(startup)
+    await dp.start_polling(bot)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    asyncio.run(main())
